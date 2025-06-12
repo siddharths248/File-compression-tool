2c6df70419cc7b08265169ba229ba2de12018ebf
siddharths248 <siddharthsgeorge248@gmail.com>
2025-06-12 12:13:24 +0530

    add bwt edge case handling

--- src/compression/algorithms/bwt/encode.py
+++ src/compression/algorithms/bwt/encode.py
@@ -1,11 +1,30 @@
 
-def bwtEncode(s : str):
+def pick_end_marker(s, pref=('$', '\0', chr(1), chr(2))):
+    for marker in pref:
+        if marker not in s:
+            return marker
+    raise ValueError("No suitable end marker found in input.")
 
-    s = s + '\0'
+
+
+def bwtEncode(s : str, endMarker: str=None):
+
+    if not isinstance(s,str):
+        raise TypeError("Input must be a string")
+    if endMarker is None:
+        endMarker = pick_end_marker(s)
+    if endMarker in s:
+        raise ValueError(f"End marker '{endMarker}' found in input")
+
+    s += endMarker
+
+    if not s:
+        return "",0
+    
     table = [s[i:] + s[:i] for i in range(len(s))]
     tableSorted = sorted(table)
     lastColumn = [row[-1] for row in tableSorted]
     ogIdx = tableSorted.index(s)
 
-    return (''.join(lastColumn),ogIdx)
+    return (''.join(lastColumn),ogIdx, endMarker)
 
